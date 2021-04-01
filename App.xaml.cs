f6be469154b71dffa8d4a73332c9b51c7c6ca564
Marcos <mmt.mateus@FCT.UNL.PT>
2021-04-01 21:44:16 +0100

    Revert "MySQL database added"
    This reverts commit 4ffb2ad7bc9ff64b4adfd7d11f8ad360b93f624a.

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,12 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-
 namespace Contact_manager {
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
 	public partial class App : Application {
-		
 	}
 }
--- RegisterWindow.xaml.cs
+++ RegisterWindow.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using MySql.Data.MySqlClient;
 
 namespace Contact_manager {
 	/// <summary>
@@ -47,56 +46,5 @@ namespace Contact_manager {
 			}
 
 		}
-
-		private bool CheckEmptyFields(String username, String password, ImageSource picture) {
-
-
-			if (username.Length==0&&password.Length==0) {
-				MessageBox.Show("Need to specify the username and password.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-				return true;
-			}
-			else if (password.Length==0) {
-				MessageBox.Show("Need to specify the password.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-				return true;
-			}
-			else if (username.Length==0) {
-				MessageBox.Show("Need to specify the username.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-				return true;
-			}
-			else if (picture == null) {
-				MessageBox.Show("Upload a picture.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-				return true;
-
-			}
-			return false;
-		}
-
-
-		
-		private void Register_Click(object sender, RoutedEventArgs e) {
-
-			string firstname=first_name.Text;
-			string lastname = last_name.Text;
-			string user = username.Text;
-			string password = TextPassword.Password;
-
-
-			//Check empty fields (namely, username and password fields)
-			if (!CheckEmptyFields(user, password, PictureBox.Source)) {
-				Database db = new Database();
-				var con = db.GetMySqlConnection;
-				db.OpenMySqlConnection();
-				var cmd = new MySqlCommand("INSERT into users(username,password) VALUES(@user,@pass)", con);
-				cmd.Parameters.Add("@user", MySqlDbType.VarChar).Value=user;
-				cmd.Parameters.Add("@pass", MySqlDbType.VarChar).Value=password;
-
-				cmd.ExecuteNonQuery();
-			}
-			
-		}	
-		
-
-
-
 	}
 }
