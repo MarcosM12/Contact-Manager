89c53c4512ac7049fb60e515033e8a2a7381985e
Marcos <mmt.mateus@FCT.UNL.PT>
2021-04-01 21:46:55 +0100

    MySQL database added

---
+++ Database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Contact_manager {
+	class Database {
+		
+		//MySQL connvection
+        private MySqlConnection conn = new MySqlConnection("datasource=localhost;user=root;database=contactmanager_database;port=3306;password=1234");
+		
+		public MySqlConnection GetMySqlConnection {
+			get { return conn; }
+
+		}
+
+		public void OpenMySqlConnection() {
+			if (conn.State==System.Data.ConnectionState.Closed) {
+
+				conn.Open();
+			}
+
+		}
+	    public void CloseMySqlConnection() {
+			if (conn.State==System.Data.ConnectionState.Open) {
+
+				conn.Close();
+			}
+
+		}
+	}
+}
