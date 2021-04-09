476dadfaa454ec0c3cd875511bf03a314c782ee4
Marcos <mmt.mateus@FCT.UNL.PT>
2021-04-09 18:59:10 +0100

    Feature to hide close button added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,18 @@ namespace Contact_manager {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
+	/// 
+
+
 	public partial class MainWindow : Window {
+
+		
+
 		public MainWindow() {
 			InitializeComponent();
+			
 		}
+
+		
 	}
 }
